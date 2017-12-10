67ec5f3731e032a8d8ac39c744a425cf878c5b43
cicinth <30847677+cicinth@users.noreply.github.com>
2017-12-10 21:00:50 -0200

    Add files via upload

---
+++ zero06/galeria.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <?php include 'imports.php'; ?>
+  </head>
+  <body>
+    <header>
+      <?php include 'menu.php'; ?>
+        
+     <footer>
+          <?php include 'rodape.php'; ?>
+    </footer>
+    </body>
+</html>
